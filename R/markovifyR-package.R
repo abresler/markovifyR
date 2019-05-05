049e9bcd075f80d10427220dc69017fa6a71a662
Alex Bresler <abresler@asbcllc.com>
2019-05-04 22:20:40 -0400

    a

--- R/markovifyR-package.R
+++ R/markovifyR-package.R
@@ -3,7 +3,7 @@
 #' @name markovifyR
 #' @docType package
 #' @author Alex Bresler (abresler@@asbcllc.com)
-#' @import dplyr purrr stringr reticulate tibble
+#' @import dplyr purrr stringr reticulate tibble furrr future
 #' @importFrom glue glue
 #' @importFrom rlang UQ
 
